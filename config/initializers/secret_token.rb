7f6139a12f4048d9c0d93776828df911f82530a3
oneilla <oneilla@grinnell.edu>
2012-09-08 11:04:48 -0500

    Updated Gemfile to use sqlite3 in development/test and pg in production/staging on Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '11fd9ca80968870350ba73ab01bc2af76b26fbc4d4c21bc1df8a135ac2491ab48a05d82cda1093911d7a71dd784be1ff0f22561c9a453c39dc2e97b9609d3705'
