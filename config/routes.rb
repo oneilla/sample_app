fe4e7b27a6c179ec76b0116731e8e798bb84aa48
oneilla <oneilla@grinnell.edu>
2012-10-03 11:39:13 -0500

    Finished Signing in - up to section 9.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 SampleApp::Application.routes.draw do
+  get "sessions/new"
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/contact', :to => 'static_pages#contact'
   match '/about',   :to => 'static_pages#about'
   match '/help',    :to => 'static_pages#help'
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
   root :to => 'static_pages#home'
 end
